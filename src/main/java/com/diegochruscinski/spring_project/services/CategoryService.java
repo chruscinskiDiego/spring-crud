fc553cfea53dabbede1ec4af19f7d01e772fa624
chruscinskiDiego <121837842+chruscinskiDiego@users.noreply.github.com>
2024-01-13 19:54:23 -0300

    Entity Category

---
+++ src/main/java/com/diegochruscinski/spring_project/services/CategoryService.java
@@ -0,0 +1,27 @@
+package com.diegochruscinski.spring_project.services;
+
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.diegochruscinski.spring_project.entities.Category;
+import com.diegochruscinski.spring_project.repositories.CategoryRepository;
+
+@Service
+public class CategoryService {
+
+	@Autowired
+	private CategoryRepository repository;
+
+	public List<Category> findAll() {		
+		return repository.findAll();
+	}
+	
+	public Category findById(Long id) {
+		Optional<Category> obj = repository.findById(id);
+		return obj.get();	
+		
+	}
+}
