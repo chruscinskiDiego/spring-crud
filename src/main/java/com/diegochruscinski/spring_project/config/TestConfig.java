0951214e470776f94dd59325c16043ee2d19a655
chruscinskiDiego <121837842+chruscinskiDiego@users.noreply.github.com>
2024-01-14 13:46:48 -0300

    Payment | One to One association

--- src/main/java/com/diegochruscinski/spring_project/config/TestConfig.java
+++ src/main/java/com/diegochruscinski/spring_project/config/TestConfig.java
@@ -11,6 +11,7 @@ import org.springframework.context.annotation.Profile;
 import com.diegochruscinski.spring_project.entities.Category;
 import com.diegochruscinski.spring_project.entities.Order;
 import com.diegochruscinski.spring_project.entities.OrderItem;
+import com.diegochruscinski.spring_project.entities.Payment;
 import com.diegochruscinski.spring_project.entities.Product;
 import com.diegochruscinski.spring_project.entities.User;
 import com.diegochruscinski.spring_project.entities.enums.OrderStatus;
@@ -77,6 +78,11 @@ public class TestConfig implements CommandLineRunner {
 		OrderItem oi4 = new OrderItem(o3, p5, 2, p5.getPrice()); 
 
 		orderItemRepository.saveAll(Arrays.asList(oi1,oi2,oi3,oi4));
+		
+		Payment pay1 = new Payment(null, Instant.parse("2019-06-20T22:53:07Z"), o1 );
+		o1.setPayment(pay1);
+		
+		orderRepository.save(o1);
 	}
 
 }
